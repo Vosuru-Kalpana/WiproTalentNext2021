5e995ae0da2aa37ad98e81dcb6a93eee935cd150
Kalpana V <kalpanavosuru@gmail.com>
2020-05-28 15:12:24 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,11 @@
+class Sum
+{
+    public static void main(String args[])
+    {
+       int x,y,sum;
+       x=Integer.parseInt(args[0]);
+       y=Integer.parseInt(args[1]);
+       sum=x+y;
+       System.out.println("sum of "+x "and"+y "is:"+sum);
+    }
+}
